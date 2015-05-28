9a58cb36d25282bc14695eda37a504c4c82a1c23
ccrespoh <carolinach@me.com>
2015-05-28 11:25:38 -0400

    index and login added

---
+++ index_tp.php
@@ -0,0 +1,19 @@
+<?php
+    require_once 'login_tp.php';
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+    <style>
+        h1{
+            border-bottom: 3px solid darkblue;
+        }
+
+    </style>
+    </head>
+<body>
+    <h1>All CoursesOnline</h1>
+</body>
+</html>

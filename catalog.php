5e2871e8b9c26aa6f20e40f3391bfad6560eb29c
ccrespoh <carolinach@me.com>
2015-05-29 14:23:42 -0400

    switch pour ouvrir le gallery de chaque catalog

--- catalog.php
+++ catalog.php
@@ -1,6 +1,6 @@
 <div id="catalog">
     <a href="?page=cloud"><img src="img/cloud_computing.jpg" alt="cloud"></a>
-    <a href="#"><img src="img/front_end.jpg" alt="front"></a>
-    <a href="#"><img src="img/ios.jpg" alt="front"></a>
-    <a href="#"><img src="img/java.jpg" alt="front"></a>
+    <a href="?page=cloud"><img src="img/front_end.jpg" alt="front"></a>
+    <a href="?page=cloud"><img src="img/ios.jpg" alt="front"></a>
+    <a href="?page=cloud"><img src="img/java.jpg" alt="front"></a>
 </div>

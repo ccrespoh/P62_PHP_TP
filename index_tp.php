75dabb81c2262c0a1031a20759e9be8d1861f0ba
Carolina <carolinach@me.com>
2015-05-30 07:32:09 -0400

    adding button log in/log off to index

--- index_tp.php
+++ index_tp.php
@@ -9,7 +9,7 @@
 <div id="wrapper">
     <?php
     require_once 'header.php';
-    require_once 'login_tp.php';
+    require_once 'test.php';
     require_once 'catalog.php';
     require_once 'footer.php';
     ?>
--- login_tp.php
+++ login_tp.php
@@ -1,8 +1,5 @@
-<?php
-echo "<nav><a href='index_tp.php' Onclick='afficher_login();'><li>Sign Up</li></a><a href='inscription.php'><li>S'inscrire</li></a> </nav>";
-?>
-<?php
 
+<?php
 define('NAME', 'admin');
 define('PASSWORD', '123');
 

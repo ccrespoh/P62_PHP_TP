9ac03651a3a198ecf9f4711e796bc227ecacb3e9
MengXue <xuemon@gmail.com>
2015-05-29 23:13:14 -0400

    resoltin de location de button ajouter

--- function.php
+++ function.php
@@ -9,7 +9,7 @@ function demander_data($cata, $tb_complet)
     $tab = array();
     foreach ($tb_complet as $val) {
         if ($val['cata'] == $cata) {
-            $tab[] = $val;            // nouveau array commence par 0
+            $tab[] = $val;            // nouveau array commence par 0 (dans [ ] est vide)
         }
     }
     return $tab;
@@ -34,7 +34,8 @@ function afficher_article($cata, $tb_cata)
         echo "<th>{$val['name']}</th><th>{$val['heures']}</th><th>{$val['personnes']}</th><th>{$val['prix']} $</th>";
         echo '</tr>';
         echo '</table>';
-        echo "<a href='?action=add&&name={$val['name']}'>Participer</a>";
+        // mise de '?page=$cata' pour s'adapter au 'switch' dans catalog.php
+        echo "<a href='?page=$cata&&action=add&&name={$val['name']}'>Participer</a>";
         echo '</div>';
     }
     echo '</div>';

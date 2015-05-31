66f5f81b7a1a46c22ac38fa9e07b9b5a175d845b
Carolina <carolinach@me.com>
2015-05-31 14:21:37 -0400

    ajouter prix

--- panier.php
+++ panier.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include_once 'data.php';
 if (!array_key_exists('info', $_SESSION)) {
     $_SESSION = array(
         'info' => array(),
@@ -9,7 +10,6 @@ if (!array_key_exists('info', $_SESSION)) {
 echo "<nav><a href='?page=login' onclick='afficher_login();'><li>Sign Up</li></a><a href='inscription.php'><li>Sinscrire</li></a></nav><br/><br/>";
 if (array_key_exists('action', $_GET) && ($_GET['action']=='add')) {
     $_SESSION['cours_choisi'][$_GET['name']] = true;  // utiliser 'key' unique (soit nom de cours) pour ne pas repeter
-    header('Location:' . "http://localhost/P62_PHP/P62_PHP_TP/index_tp.php?page={$_GET['page']}");
 }
 if (array_key_exists('action', $_GET) && ($_GET['action']=='remove')) {
    unset($_SESSION['cours_choisi'][$_GET['name']]);  // utiliser 'key' unique (soit nom de cours) pour ne pas repeter
@@ -20,7 +20,8 @@ if (array_key_exists('action', $_GET) && ($_GET['action']=='remove')) {
 <aside>
 <h3>Panier</h3>
 <ul>
-<?php foreach ($_SESSION['cours_choisi'] as $name=>$val){ echo "<li>$name</li><a href='?action=remove&&name=$name'><img src='images/button_x.png' alt='x'/></a>";}?>
+<?php foreach ($_SESSION['cours_choisi'] as $name=>$val){
+ echo "<li>$name</li><a href='?action=remove&&name=$name'><img src='images/button_x.png' alt='x'/></a>";}?>
 </ul>
 </aside>
 
